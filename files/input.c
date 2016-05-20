ed9c9f7449ac568416ca23633fd5f5728a932190
Jordao Serafim <jordaoesa@gmail.com>
2016-05-20 09:33:30 -0300

    arquivo gerado pelo professor

---
+++ files/input.c
@@ -0,0 +1,31 @@
+
+int max(int a, int b){
+	
+	a = b + 1;
+	
+	return a;
+}
+
+void main(){
+	int x1 = 0;
+	int a=1;
+	if(1 > a){
+		x1 = 1;
+		int y = x1+1;
+	}else {
+	    int rr = 2 *3;
+	    int dd = rr;
+	    int ff = max(3,4);
+	}
+	
+	int b;
+	_Bool bb = 0;
+	_Bool aa = 1;
+    float f = 1.1;
+    
+	a = max(2, 3);
+	if( aa == 1 ) {
+		int c = b;
+	}
+}
+
